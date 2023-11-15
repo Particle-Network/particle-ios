b7a88488075f87c690f1d12ea6c31a297c547ffa
Zhichao Link <sunzhichao@minijoy.work>
2023-11-15 14:34:42 +0800

    update DemoObjc

--- DemoObjc/DemoObjc/ParticleConnectWrapper.swift
+++ DemoObjc/DemoObjc/ParticleConnectWrapper.swift
@@ -9,17 +9,16 @@ import ConnectCommon
 import ConnectEVMAdapter
 import ConnectPhantomAdapter
 import ConnectSolanaAdapter
-import ParticleAuthAdapter
 import ConnectWalletConnectAdapter
 import Foundation
+import ParticleAuthAdapter
 import ParticleConnect
 import ParticleNetworkBase
+import ParticleWalletConnect
 import ParticleWalletGUI
 
 public class ParticleConnectWrapper: NSObject {
     @objc public static func initParticleConnect() {
-        
-
         var adapters: [ConnectAdapter] = [
             MetaMaskConnectAdapter(),
             ParticleAuthAdapter(),
@@ -41,14 +40,12 @@ public class ParticleConnectWrapper: NSObject {
              Inch1ConnectAdapter.self,
              ZengoConnectAdapter.self,
              AlphaConnectAdapter.self,
-             OKXConnectAdapter.self
-            ]
+             OKXConnectAdapter.self]
 
         adapters.append(contentsOf: moreAdapterClasses.map {
             $0.init()
         })
 
-        
         ParticleConnect.initialize(env: .debug, chainInfo: .ethereum(.mainnet), dAppData: .standard) {
             adapters
         }
@@ -57,8 +54,16 @@ public class ParticleConnectWrapper: NSObject {
         ParticleWalletGUI.setShowTestNetwork(false)
         ParticleWalletGUI.setSupportChain([.bsc, .arbitrum, .harmony])
         ParticleWalletGUI.setShowManageWallet(true)
+        // Initialize particle wallet connect sdk
+        ParticleWalletConnect.initialize(
+            WalletMetaData(name: "Particle Wallet",
+                           icon: URL(string: "https://connect.particle.network/icons/512.png")!,
+                           url: URL(string: "https://particle.network")!,
+                           description: nil))
+        // set wallet connect v2 project id to ParticleWalletConnect, used when connect as a wallet.
+        ParticleWalletConnect.setWalletConnectV2ProjectId("75ac08814504606fc06126541ace9df6")
     }
-    
+
     @objc public static func handleUrl(url: URL) -> Bool {
         return ParticleConnect.handleUrl(url)
     }
